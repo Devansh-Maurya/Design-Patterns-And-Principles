c2bad264a1a5ccc800d90799ec9adfd221272627
Devansh Maurya <Devansh233@gmail.com>
2020-09-12 20:46:44 +0530

    Add macro command example

--- src/command/RemoteLoader.kt
+++ src/command/RemoteLoader.kt
@@ -1,9 +1,6 @@
 package command
 
-import command.command.LightOffCommand
-import command.command.LightOnCommand
-import command.command.StereoOffCommand
-import command.command.StereoOnWithCDCommand
+import command.command.*
 import command.invoker.RemoteControl
 import command.receiver.Light
 import command.receiver.Stereo
@@ -29,10 +26,14 @@ fun main() {
     val stereoOnWithCD = StereoOnWithCDCommand(stereo)
     val stereoOff = StereoOffCommand(stereo)
 
+    val partyOnMacro = MacroCommand(listOf(livingRoomLightOn, stereoOnWithCD))
+    val partyOffMacro = MacroCommand(listOf(livingRoomLightOff, stereoOff))
+
     remoteControl.run {
         setCommand(0, livingRoomLightOn, livingRoomLightOff)
         setCommand(1, kitchenLightOn, kitchenLightOff)
         setCommand(2, stereoOnWithCD, stereoOff)
+        setCommand(3, partyOnMacro, partyOffMacro)
 
         println(this)
 
@@ -42,5 +43,10 @@ fun main() {
         offButtonPushed(1)
         onButtonPushed(2)
         offButtonPushed(2)
+
+        println("--- Pushing Macro On ---")
+        onButtonPushed(3)
+        println("--- Pushing Macro Off ---")
+        offButtonPushed(3)
     }
 }
